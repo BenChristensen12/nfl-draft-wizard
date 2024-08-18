3169ac711bfcfe887982fb16a6f4d60bbfd978c9
Ben Christensen <benjc1212@gmail.com>
2024-08-18 17:49:33 -0400

    first crack
    First attempt at making the dashboard

---
+++ Wizard.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from utils.functions import *
+
+if "data" not in st.session_state:
+    st.session_state["data"] = load_players()
+    st.session_state["roster"] = dict()
+    st.session_state["draft_begun"] = False
+
+def begin_draft():
+    positions = st.session_state.roster.keys()
+    st.session_state.data = st.session_state.data[st.session_state.data.position.isin(positions)].copy()
+    st.session_state["draft"] = Draft(st.session_state.player_num, st.session_state.data, st.session_state.roster)
+    for position in positions:
+        if position == "FLEX":
+            data = st.session_state.data[st.session_state.data.position.isin(["WR", "RB", "TE"])].copy()
+        else:
+            data = st.session_state.data[st.session_state.data.position == position].copy()
+        data.sort_values("projection", inplace = True, ascending = False)
+        data.insert(0, 'Rank', range(1, len(data) + 1))
+        st.session_state[position] = data.copy()        
+        create_newpage(position)   
+    num_players = st.session_state.player_num
+    half_players = num_players / 2
+    st.session_state.data["differential"] = st.session_state.data.groupby("position").projection.apply(lambda x: x.shift(-half_players).rolling(window=half_players).mean()
+    st.session_state["draft_order"] = [i for i in range(1, num_players+1)] + [i for i in range(num_players, 0, -1)] 
+    st.session_state.draft_begun = True
+
+st.title("NFL Draft Wizard")
+if not st.session_state.draft_begun:
+    st.number_input("How many teams in your draft?", min_value = 1, key = "player_num")
+    st.number_input("What number are you?", min_value = 1, key = "your_num")
+    position_choices = st.multiselect("What positions are on the roster?", options = ["QB", "RB", "WR", "TE", "FLEX", "K", "DST"])
+    for choice in position_choices:
+        position_number = st.number_input(choice, min_value = 1)
+        st.session_state.roster[choice] = position_number
+    st.button("Enter Draft!", on_click = begin_draft)
+
+else:
+    for i in range(st.session_state.player_num):
+        st.session_state[f"Team_{i+1}"] = Team(i+1)
+    up = st.session_state.draft_order[0]
+    on_deck = st.session_state.draft_order[1]
+    if up == st.session_state.your_num:
+        st.header("Your pick!")
+    else:
+        st.header(f"Team {up} is up")
+    if on_deck == st.session_state.your_num:
+        st.header("Your pick next!")
+    else:
+        st.header(f"Team {on_deck} is up next")
+
+
+    #Put where it needs to go:
+    # st.session_state.draft_order.append(st.session_state.draft.next_picks.pop(0))
+
+    
---
+++ utils/functions.py
@@ -0,0 +1,60 @@
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import os
+import pandas as pd
+import numpy as np
+
+repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+class Draft():
+    def __init__(self, num_players, data, positions):
+        self.players = num_players
+        self.data = data
+        self.positions = positions
+
+class Team():
+    def __init__(self, order):
+        self.order = order
+        self.roster = []
+
+    def pick(self, player, df):
+        self.roster.append(player)
+        self.data = df[df.Player.isin(self.roster)].copy()
+
+
+def load_players():
+    df = pd.DataFrame()
+    for position in ["QB", "RB", "WR", "TE", "K", "DST"]:
+        position_df = pd.read_csv(repo_dir + f"/utils/data/FantasyPros_Fantasy_Football_Projections_{position}.csv")
+        position_df["position"] = position
+        if pd.isna(position_df.iloc[0,2]):
+            position_df = position_df.iloc[1:].copy()
+        position_df["type"] = None
+        position_df.loc[position_df.Player.isna(), "type"] = position_df.loc[position_df.Player.isna(), "Team"]
+        position_df.type = position_df.type.fillna("projection")
+        position_df.loc[position_df.Team.isin(["high", "low"]), "Team"] = None
+        if position == "DST":
+            position_df.loc[~position_df.Player.isna(), "Player"] = position_df.loc[~position_df.Player.isna(), "Player"].apply(lambda x: x.split("high")[0])
+        else:
+            position_df.loc[~position_df.Team.isna(), "Team"] = position_df.loc[~position_df.Team.isna(), "Team"].apply(lambda x: x.split("high")[0])
+        position_df.Player = position_df.Player.fillna(method = "ffill")
+        position_df.Team = position_df.Team.fillna(method = "ffill")
+        df = pd.concat([df, position_df])
+    df = df.loc[~df.FPTS.isna(), ["Player", "Team", "FPTS", "position", "type"]]
+    df = df.pivot(index = ["Player", "Team", "position"], columns = "type", values = "FPTS").reset_index()
+    df["picked"] = 0
+    df.sort_values("projection", ascending = False, inplace = True)
+    df["differential"] = df.groupby("position").projection.apply(lambda x: x.shift(-5).rolling(window = 5).mean())
+    return df[["Player", "Team", "position", "low", "projection", "high", "picked"]]
+
+def create_newpage(position):
+    code = f"""
+import streamlit as st
+from utils.functions import *
+st.dataframe(st.session_state.position)
+"""
+    with open(repo_dir + f"/pages/{position}.py", 'w') as file:
+        file.write(code)
+
