ca16cd69cbe0d68c0e4d1d3b29f2d03935e1bca1
Ben Christensen <benjc1212@gmail.com>
2024-08-31 22:58:47 -0400

    add data_editor method for picking players
    Next: Remove FLEX from the front-end and only infer it indirectly?

--- Wizard.py
+++ Wizard.py
@@ -10,7 +10,7 @@ if "data" not in st.session_state:
     st.session_state["pick_number"] = 0
 
 def calculate_lead(pick_num, score = "projection"):
-    data = st.session_state.data.copy()    
+    data = st.session_state.data.sort_values([score], ascending = False)   
     team = st.session_state.draft_order[pick_num]
     window = round(st.session_state.picks_between[pick_num]/2)
     roster = st.session_state[f"team_{team}_likely_roster"]
@@ -40,8 +40,8 @@ def begin_draft():
 
 def enact_pick():
     team = st.session_state.draft_order[st.session_state.pick_number]
-    player, position = st.session_state.pick.split(":")
-    st.session_state[f"team_{team}_roster"][position].append(st.session_state.pick)
+    player, position = st.session_state.pick.Player.values[0], st.session_state.pick.position.values[0]
+    st.session_state[f"team_{team}_roster"][position].append(player)
     st.session_state[f"team_{team}_picks"].append(player)
     st.session_state.all_picks.append(player)
     st.session_state.data.loc[st.session_state.data.Player == player, "picked"] = 1
@@ -55,7 +55,7 @@ if not st.session_state.draft_begun:
         st.session_state[f"team_{i+1}_name"] = st.text_input(f"Team {i + 1} Name:")
     your_num = st.number_input("What number are you?", min_value = 1)
     st.session_state["num_players"] = num_players
-    st.session_state["your_num"] = your_num
+    st.session_state["your_num"] = your_num 
     position_choices = st.multiselect("What positions are on the roster?", options = ["QB", "RB", "WR", "TE", "FLEX", "K", "DST"])
     for choice in position_choices:
         position_number = st.number_input(choice, min_value = 1)
@@ -64,28 +64,34 @@ if not st.session_state.draft_begun:
 
 else:
     team = st.session_state.draft_order[st.session_state.pick_number]
-    if st.session_state.pick_number == st.session_state.your_num:
+    if st.session_state.pick_number+1 == st.session_state.your_num:
         st.header("Your pick!")
     else:
         st.header(f"Team {st.session_state[f'team_{team}_name']} is up")
+    score = st.selectbox("Select Score to Optimize", options = ["low", "projection", "high"], index = 1)
     tabs = st.tabs(["Recommendation", "QB", "RB", 'WR', 'TE', 'FLEX', 'K', 'D/ST'])
-    positions = ["QB", "RB", 'WR', 'TE', 'FLEX', 'K', 'D/ST']
     with tabs[0]:
         st.subheader("Recommended Pick")
-        score = st.selectbox("Select Score to Optimize", options = ["low", "projection", "high"], index = 1)
         df = calculate_lead(st.session_state.pick_number, score)
         agg_dict ={"Player": "first", "lead": "first", score: "first"}
         highest_lead = df.groupby("position", as_index = False).agg(agg_dict).sort_values("lead", ascending = False)
         highest_pts = df.groupby("position", as_index = False).agg(agg_dict).sort_values([score], ascending = False)
         # df["Rank"] = df.groupby("position")[score].rank(method = 'dense', ascending = False)
         # df.sort_values(["Rank"], inplace = True, ascending = True)
-        st.dataframe(highest_lead[["position", "Player", score, "lead"]].round(), hide_index = True)
-            
+        highest_lead = highest_lead[["position", "Player", score, "lead"]].round().copy()
+        highest_lead.insert(0, "Pick", False)
+        edited_df = st.data_editor(highest_lead, disabled = ["position", "Player", score, "lead"], hide_index = True)
+        if edited_df.Pick.sum() > 0:
+            st.session_state.pick = edited_df.loc[edited_df.Pick, ["Player", "position"]]
+    positions = ["QB", "RB", 'WR', 'TE', 'FLEX', 'K', 'DST']            
     for i, tab in enumerate(tabs[1:]):
         with tab:
             position = positions[i]
-            st.dataframe(df[df.position == position].sort_values(score, ascending = False).round())
-    st.selectbox(f"Team {st.session_state[f'team_{team}_name']} picks", options = df["Player"] + ":" + df["position"], key = "pick", index = None)
+            position_df = df.loc[df.position == position, ["Player", "low", "projection", "high", "lead"]].sort_values(score, ascending = False).round()
+            position_df.insert(0, "Pick", False)
+            edited_df = st.data_editor(position_df, disabled = ["Player", "low", "projection", "high", "lead"], hide_index = True)
+            if edited_df.Pick.sum() > 0:
+                st.session_state.pick = df.loc[df.Player == edited_df.loc[edited_df.Pick, "Player"].values[0], ["Player", "position"]]
     st.button("Enact Pick!", on_click = enact_pick)
     st.header("Your Team")
     st.write(st.session_state[f"team_{st.session_state.your_num}_roster"])
