499a8cb69a35e434fb52df7c802efb7d34102519
Ben Christensen <benjc1212@gmail.com>
2025-08-22 09:39:36 -0400

    get data from ESPN through API instead of static excel files; put roster in sidebar; implement drafting with check boxes and no submit button

--- Wizard.py
+++ Wizard.py
@@ -1,82 +1,75 @@
 import streamlit as st
+from time import time
 from utils.functions import *
 
+st.set_page_config(layout="wide")
+
 if "data" not in st.session_state:
-    st.session_state["data"] = load_players()
-    st.session_state["position_counts"] = dict()
-    st.session_state["positions"] = ["QB", "RB", "WR", "TE", "K", "DST"]
-    st.session_state["draft_begun"] = False
-    st.session_state["rounds"] = 16
-    st.session_state["all_picks"] = []
-    st.session_state["pick_number"] = 0
-    st.session_state["denominators"] = {"QB": 4, "RB": 2, "WR": 2, "TE": 4, "K": 8, "DST": 8}
-
-def calculate_lead(pick_num, score = "projection"):
-    data = st.session_state.data.sort_values([score], ascending = False)   
-    df = pd.DataFrame()
-    for position in st.session_state.chosen_positions:
-        denominator = st.session_state.denominators[position]
-        window = max(round(st.session_state.picks_between[pick_num]/denominator), 1)
-        position_df = data[(data.position == position) & ~data.Pick].copy()
-        position_df["lead"] = position_df[score] - position_df[score].shift(-window)
-        df = pd.concat([df, position_df])
-    df = data.merge(df, how = 'left', on = ["position", "Player", "Team", "low", "projection", "high"])
-    return df
-
-def begin_draft():
-    st.session_state["roster"] = dict(zip(st.session_state.positions, [[] for position in st.session_state.positions]))
-    num_players = st.session_state.num_players
-    st.session_state["draft_order"] = int(st.session_state.rounds/2) * ([i for i in range(1, num_players+1)] + [i for i in range(num_players, 0, -1)])
-    i, n = st.session_state.your_num, st.session_state.num_players
-    repeats = int(st.session_state.rounds / 2)
-    st.session_state["your_pick_order"] = sorted([2*k*n + i for k in range(repeats)] + [2*k*n + 2*n + 1-i for k in range(repeats)])
-    st.session_state["picks_between"] = st.session_state.rounds * [2*(num_players-i) if i < num_players else 2*(num_players-1) for i in range(1, num_players+1)]       
-    st.session_state["draft_begun"] = True
+    initialize_dashboard()
 
 
-st.title("NFL Draft Wizard")
 if not st.session_state.draft_begun:
-    st.session_state["num_players"] = st.number_input("How many teams in your draft?", min_value = 1)
-    st.session_state["your_num"] = st.number_input("What number are you in the draft?", min_value = 1)
-
-    chosen_positions = st.multiselect("What positions are on the roster?", options = st.session_state.positions)
-    st.session_state["chosen_positions"] = chosen_positions
-    st.button("Enter Draft!", on_click = begin_draft)
+    st.title("NFL Draft Wizard")
+    prompt_draft_details()
 
 else:
-    if st.session_state.pick_number+1 == st.session_state.your_num:
-        st.header("Round y | It is your pick!")
+    with st.sidebar:
+        st.header("Your Picks")
+        st.dataframe(st.session_state.roster, hide_index = True)
+    pick_number = st.session_state.data.Taken.sum()
+    if pick_number+1 in st.session_state.your_pick_order:
+        st.markdown(
+            "<h1 style='background-color:yellow; color:black; padding:10px; border-radius:10px;'>"
+            "⚡ Your Pick!"
+            "</h1>",
+            unsafe_allow_html=True
+        )
     else:
-        st.header(f"Round y | x picks to go")
-    score = st.selectbox("Select Score to Optimize", options = ["low", "projection", "high"], index = 1)
+        st.title("NFL Draft Wizard")
+
     st.subheader("Recommended Pick")
-    df = calculate_lead(st.session_state.pick_number, score)
-    agg_dict ={"Player": "first", "lead": "first", score: "first"}
-    highest_lead = df.groupby("position", as_index = False).agg(agg_dict).sort_values("lead", ascending = False)
-    highest_pts = df.groupby("position", as_index = False).agg(agg_dict).sort_values([score], ascending = False)
-    highest_lead = highest_lead[["position", "Player", score, "lead"]].round().copy()
+    at_risk_rows = st.session_state.data[~st.session_state.data.Taken].head(st.session_state.picks_between[pick_number]).index
+    st.session_state.data.loc[at_risk_rows, "at_risk"] = True
+    max_projections = st.session_state.data[~st.session_state.data.Taken].groupby("position").projection.max()
+    leads =  max_projections - st.session_state.data[(~st.session_state.data.Taken)& (~st.session_state.data.at_risk)].groupby("position").projection.max()    
+    for position in st.session_state.positions:
+        if position != "Flex":
+            st.session_state.data.loc[(~st.session_state.data.Taken) & (st.session_state.data.projection == max_projections[position]), "lead"] = leads[position]
+    agg_dict ={"Player": "first", "lead": "first", "projection": "first", "at_risk": "first"}
+    highest_lead = st.session_state.data[(~st.session_state.data.Taken)].sort_values("projection", ascending = False).groupby("position", as_index = False).agg(agg_dict).sort_values(["lead", "projection"], ascending = False)
+    highest_lead = highest_lead[["position", "Player", "projection", "lead", "at_risk"]].round().copy()
     st.dataframe(highest_lead, hide_index = True)    
-    positions = st.session_state.chosen_positions    
+    positions = st.session_state.chosen_positions  
+    st.subheader(f"Current Pick: {pick_number+1}")  
     tabs = st.tabs(["All"] + positions)
     picked = set()
     for i, tab in enumerate(tabs):
         with tab:
-            if i > 0:
-                position = positions[i-1]
-                data_df = df.loc[df.position == position, ["Pick", "Player", "low", "projection", "high", "lead"]].sort_values(score, ascending = False).round()
+            if i == 0:
+                data_df = st.session_state.data.sort_values(["Taken", "draft_position"])[["Taken", "position", "Player", "projection", "lead", "at_risk"]].round()
+            elif positions[i-1] == "Flex":
+                data_df = st.session_state.data.sort_values(["Taken", "draft_position"]).loc[st.session_state.data.position.isin(["WR", "RB", "TE"]), ["Taken", "Player", "projection", "lead", "at_risk", "outlook"]].round()
+                lead = data_df.projection.max() - data_df.loc[~data_df.at_risk, "projection"].max()
+                data_df["lead"] = None
+                data_df.sort_values("projection", ascending = False, inplace = True)
+                data_df.loc[0, "lead"] = lead
             else:
-                data_df = df[["Pick", "position", "Player", "low", "projection", "high", "lead"]].sort_values(score, ascending = False).round()
-            edited_df = st.data_editor(data_df, disabled = ["position", "Player", "low", "projection", "high", "lead"], hide_index = True)
-            picked.update(edited_df.loc[df.Pick, "Player"].values)
+                position = positions[i-1]
+                data_df = st.session_state.data.sort_values(["Taken", "draft_position"]).loc[st.session_state.data.position == position, ["Taken", "Player", "projection", "lead", "at_risk", "outlook"]].round()
+                
+            edited_df = st.data_editor(data_df, disabled = ["position", "Player", "projection", "lead", "at_risk"], hide_index = True, use_container_width=True)
+            picked.update(edited_df.loc[edited_df.Taken, "Player"].values)
     picked = list(picked)
-    st.session_state.data.Pick = 0
-    st.session_state.data.loc[st.session_state.data.Player.isin(picked), "Pick"] = 1
+    if len(picked) != st.session_state.data.Taken.sum():
+        if pick_number+1 in st.session_state.your_pick_order:
+            player_picked = list(set(picked) - set(st.session_state.data.loc[st.session_state.data.Taken, "Player"].values))[0]
+            row = st.session_state.data[st.session_state.data.Player == player_picked].iloc[0]
+            update_roster_with_roster_df(row, pick_number)
+        st.session_state.data.Taken = False
+        st.session_state.data.lead = None
+        st.session_state.data.loc[st.session_state.data.Player.isin(picked), "Taken"] = True
+        st.rerun()
 
-
-st.session_state
-
-
-
-    
-# For each position, don't hide any players, just have the checkbox mean the player has been picked
-# On recommendation page, hide flex
+# Fix Flex tab
+# Allow picking from recommended pick table
+# Enforce player limits so you can't pick more than allowed
--- utils/functions.py
+++ utils/functions.py
@@ -1,28 +1,123 @@
 import os
 import pandas as pd
 import numpy as np
+import requests, json
+import streamlit as st
 
-repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-def load_players():
-    df = pd.DataFrame()
-    for position in ["QB", "RB", "WR", "TE", "K", "DST"]:
-        position_df = pd.read_csv(repo_dir + f"/utils/data/FantasyPros_Fantasy_Football_Projections_{position}.csv")
-        position_df["position"] = position
-        if pd.isna(position_df.iloc[0,2]):
-            position_df = position_df.iloc[1:].copy()
-        position_df["type"] = None
-        position_df.loc[position_df.Player.isna(), "type"] = position_df.loc[position_df.Player.isna(), "Team"]
-        position_df.type = position_df.type.fillna("projection")
-        position_df.loc[position_df.Team.isin(["high", "low"]), "Team"] = None
-        if position == "DST":
-            position_df.loc[~position_df.Player.isna(), "Player"] = position_df.loc[~position_df.Player.isna(), "Player"].apply(lambda x: x.split("high")[0])
+def initialize_dashboard():
+    st.session_state["data"] = retrieve_espn_api()
+    st.session_state.data["Taken"] = False
+    st.session_state.data["at_risk"] = False
+    st.session_state["position_counts"] = dict()
+    st.session_state["positions"] = ["QB", "RB", "WR", "TE", "Flex", "K", "DST"]
+    st.session_state["draft_begun"] = False
+    st.session_state["rounds"] = 16
+    st.session_state["all_picks"] = []
+    st.session_state["pick_number"] = 0
+    st.session_state["denominators"] = {"QB": 4, "RB": 2, "WR": 2, "TE": 4, "K": 8, "DST": 8}
+    st.session_state["round"] = 1    
+
+def prompt_draft_details():
+    st.session_state["num_players"] = st.number_input("How many teams in your draft?", min_value = 2)
+    st.session_state["your_num"] = st.number_input("What number are you in the draft?", min_value = 1)
+
+    chosen_positions = st.multiselect("What positions are on the roster?", options = st.session_state.positions)
+    st.session_state["chosen_positions"] = chosen_positions
+    if chosen_positions:
+        for position in chosen_positions:
+            position_count = st.number_input(f"How many {position}?", min_value = 1)
+            st.session_state.position_counts[position] = position_count
+        bench_count = st.number_input("How many on the bench?", min_value = 1)
+        st.session_state["bench_count"] = bench_count
+    st.button("Enter Draft!", on_click = begin_draft)    
+
+def begin_draft():
+    roster = pd.DataFrame(columns = ["Position", "Player", "Projection", "Average Draft Position", "Your Draft Position", "Season Outlook"])
+    for position in st.session_state.chosen_positions:
+        for i in range(st.session_state.position_counts[position]):
+            roster.loc[len(roster)] = [position, None, None, None, None, None]
+    for i in range(st.session_state.bench_count):
+        roster.loc[len(roster)] = ["Bench", None, None, None, None, None]
+    st.session_state["roster"] = roster.copy()
+    y, n = st.session_state.your_num, st.session_state.num_players
+    repeats = int(st.session_state.rounds / 2)
+    st.session_state["draft_order"] = repeats * ([i for i in range(1, n+1)] + [i for i in range(n, 0, -1)])
+    st.session_state["your_pick_order"] = sorted([2*k*n + y for k in range(repeats)] + [2*k*n + 2*n + 1-y for k in range(repeats)])
+    st.session_state["picks_between"] = st.session_state.rounds * [2*(n-i) if i < n else 2*(n-1) for i in range(1, n+1)]
+    
+    st.session_state["draft_begun"] = True
+
+def update_roster_with_roster_df(row, pick_number):
+    position = row["position"]
+    roster_position_df = st.session_state.roster[st.session_state.roster.Player.isna() & (st.session_state.roster.Position == position)].copy()
+    if len(roster_position_df) > 0:
+        st.session_state.roster.loc[roster_position_df.index[0]] = [row["position"], row["Player"], row["projection"], row["draft_position"], pick_number+1, row["outlook"]]
+    elif position in ["RB", "WR", "TE"]:
+        roster_position_df = st.session_state.roster[st.session_state.roster.Player.isna() & (st.session_state.roster.Position == "Flex")].copy()
+        if len(roster_position_df) > 0:
+            st.session_state.roster.loc[roster_position_df.index[0]] = ["Flex", row["Player"], row["projection"], row["draft_position"], pick_number+1, row["outlook"]]
         else:
-            position_df.loc[~position_df.Team.isna(), "Team"] = position_df.loc[~position_df.Team.isna(), "Team"].apply(lambda x: x.split("high")[0])
-        position_df.Player = position_df.Player.fillna(method = "ffill")
-        position_df.Team = position_df.Team.fillna(method = "ffill")
-        df = pd.concat([df, position_df])
-    df = df.loc[~df.FPTS.isna(), ["Player", "Team", "FPTS", "position", "type"]]
-    df = df.pivot(index = ["Player", "Team", "position"], columns = "type", values = "FPTS").reset_index()
-    df.insert(0, "Pick", False)
-    df.sort_values("projection", ascending = False, inplace = True)
-    return df[["Pick", "position", "Player", "Team", "low", "projection", "high"]]
+            roster_position_df = st.session_state.roster[st.session_state.roster.Player.isna() & (st.session_state.roster.Position == "Bench")].copy()
+            st.session_state.roster.loc[roster_position_df.index[0]] = ["Bench", row["Player"], row["projection"], row["draft_position"], pick_number+1, row["outlook"]]
+    else:
+        roster_position_df = st.session_state.roster[st.session_state.roster.Player.isna() & (st.session_state.roster.Position == "Bench")].copy()
+        st.session_state.roster.loc[roster_position_df.index[0]] = ["Bench", row["Player"], row["projection"], row["draft_position"], pick_number+1, row["outlook"]]
+
+def fetch_all_players():
+    url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/2025/segments/0/leaguedefaults/3?view=kona_player_info"
+    all_players = []
+    offset = 0
+    page_size = 200  # safe chunk; bump if you like
+    while True:
+        xff = {
+            "players": {
+                "limit": page_size,
+                "offset": offset,
+                "sortPercOwned": {"sortPriority": 1, "sortAsc": False}
+            }
+        }
+        headers = {
+            "Accept": "application/json",
+            "X-Fantasy-Filter": json.dumps(xff),
+        }
+        r = requests.get(url, headers=headers, verify=False)
+        r.raise_for_status()
+        page = r.json().get("players", [])
+        if not page:
+            break
+        all_players.extend(page)
+        offset += len(page)
+        return all_players
+
+def retrieve_espn_api():
+    repo_dir = os.path.dirname(os.path.abspath(__file__))
+    config = json.load(open(repo_dir + "/config.json"))    
+    players = fetch_all_players()
+    names, positions, draft_positions, outlooks, projections = [], [], [], [], []
+    for player_dict in players:
+        names.append(player_dict['player']['fullName'])
+        positions.append(config["defaultPositionId"][str(player_dict['player']['defaultPositionId'])])
+        draft_positions.append(player_dict['player']['ownership']['averageDraftPosition'])
+        if 'seasonOutlook' in player_dict['player'].keys():
+            outlook = player_dict['player']['seasonOutlook']
+        else: 
+            outlook = None
+        outlooks.append(outlook)
+        for stat_dict in player_dict['player']['stats']:
+            if stat_dict['externalId'] == '2025':
+                projections.append(stat_dict["appliedTotal"])
+                break
+
+    espn_data = pd.DataFrame({"Player": names,
+                              "position": positions,
+                              "draft_position": draft_positions,
+                              "outlook": outlooks,
+                              "projection": projections})
+    
+    return espn_data.sort_values("draft_position", ignore_index = True)
+
+
+
+
+
+
